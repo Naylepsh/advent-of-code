2b81042d5a0336fe9065d8c123260e2f20ca9297
sebastian.kondraciuk <sebastian.kondraciuk@irdeto.com>
2023-12-08 13:59:41 +0100

    lmao

--- 2023/day8.scala
+++ 2023/day8.scala
@@ -57,11 +57,50 @@ object day8:
 
     aux(startLabel, directions, 0)
 
+  def gcd(a: Long, b: Long): Long = if b == 0 then a else gcd(b, a % b)
+  def lcm(a: Long, b: Long): Long = (a * b) / gcd(a, b)
+  def lcm(numbers: Seq[Long]): Long =
+    numbers.reduce((a, b) => lcm(a, b))
+
+  def move2(
+      startLabel: String,
+      endSuffix: String,
+      paths: List[Path],
+      directions: List[Direction]
+  ): Long =
+    @tailrec
+    def aux(
+        currentLabel: String,
+        directions: List[Direction],
+        stepCount: Long
+    ): Long =
+      if currentLabel.endsWith(endSuffix) then stepCount
+      else
+        val path = paths.find(_.label == currentLabel).get
+        val nextLabel = directions.head match
+          case Direction.L => path.left
+          case Direction.R => path.right
+        aux(nextLabel, directions.rotate, stepCount + 1)
+
+    aux(startLabel, directions, 0)
+
+  def solve2(paths: List[Path], directions: List[Direction]): Long = 
+    paths
+      .filter(_.label.endsWith("A"))
+      .map: path =>
+        move2(path.label, "Z", paths, directions)
+      .pipe(lcm)
+
 @main def runDay8 =
   import day8.*
 
   val lines = scala.io.Source.fromFile("./day8.input").getLines.toList
+  val (directions, paths) = parse(lines)
 
   // part 1
-  val (directions, paths) = parse(lines)
   move("AAA", "ZZZ", paths, directions).pipe(println)
+
+  // part 2
+  solve2(paths, directions).pipe(println)
+
+
