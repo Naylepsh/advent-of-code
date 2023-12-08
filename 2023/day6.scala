43c563d8c47af89e3edf387c18e2407580ec823a
naylepsh <naylepsh@gmail.com>
2023-12-08 20:57:22 +0100

    use Long instead of BigInt+spire for day6

--- 2023/day6.scala
+++ 2023/day6.scala
@@ -1,44 +1,39 @@
 import scala.util.chaining.*
-import spire._
-import spire.math._
-import spire.implicits._
-import spire.math.Fractional.RationalIsFractional
 
-def parse(lines: List[String]): List[(BigInt, BigInt)] =
+def parse(lines: List[String]): List[(Long, Long)] =
   lines match
     case times :: distances :: Nil =>
       getNumbers(times).zip(getNumbers(distances))
     case _ => List.empty
 
-private def getNumbers(line: String): List[BigInt] =
+private def getNumbers(line: String): List[Long] =
   val xs = line.split(":").tail.head.split("\\s+").toList match
     case "" :: tail => tail
     case xs         => xs
-  xs.map(BigInt(_))
+  xs.map(_.toLong)
 
-def wayCount(time: BigInt, distance: BigInt): BigInt =
+def wayCount(time: Long, distance: Long): Long =
   // f(x) = -x^2 + tx - d > 0
   val delta = time * time - 4 * distance
   if delta < 0 then 0
   else if delta == 0 then (time / -2).abs
   else
-    val deltaRoot = Rational(delta).sqrt
-    val t = Rational(time)
-    val x1 = ((t - deltaRoot) / 2).toDouble
-    val x2 = ((t + deltaRoot) / 2).toDouble
+    val deltaRoot = Math.sqrt(delta)
+    val x1 = ((time - deltaRoot) / 2).toDouble
+    val x2 = ((time + deltaRoot) / 2).toDouble
     val res = (x2.toInt - x1.toInt).abs
     if x1 % 1 == 0 && x2 % 1 == 0 then res - 1 else res
 
-extension (xs: List[BigInt]) def product: BigInt = xs.foldLeft(BigInt(1))(_ * _)
+extension (xs: List[Long]) def product: Long = xs.foldLeft(1L)(_ * _)
 
-def solve(lines: List[String]): BigInt =
+def solve(lines: List[String]): Long =
   parse(lines)
     .map: (time, distance) =>
       wayCount(time, distance)
     .product
 
 @main def runDay6: Unit =
-  val lines = scala.io.Source.fromFile("./day6.input.bk").getLines.toList
+  val lines = scala.io.Source.fromFile("./day6.input").getLines.toList
   // part 1
   lines.pipe(solve.andThen(println))
   // part 2
--- 2023/project.scala
+++ 2023/project.scala
@@ -1,3 +1,2 @@
-//> using dep org.typelevel::spire::0.18.0
 //> using test.dep org.scalameta::munit::0.7.29
 //> using option -Yrangepos
