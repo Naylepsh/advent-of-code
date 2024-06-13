17d0328a1715606b64986ebd4ca1ff40f45df689
naylepsh <naylepsh@gmail.com>
2024-06-13 18:20:46 +0200

    solve day14p1

---
+++ 2023/day14.scala
@@ -0,0 +1,64 @@
+package day14
+
+import cats.syntax.all.*
+
+import scala.annotation.tailrec
+import scala.util.chaining.*
+
+type Tile = '.' | '#' | 'O'
+object Tile:
+  def of: Char => Option[Tile] =
+    case v @ ('.' | '#' | 'O') => Some(v)
+    case _                     => None
+
+type Platform = List[List[Tile]]
+object Platform:
+  val empty: Platform = List.empty[List[Tile]]
+
+def parse(input: List[String]): Option[Platform] =
+  val initial: Option[Platform] = Platform.empty.some
+  input
+    .foldLeft(initial): (acc, line) =>
+      val xs: Option[List[Tile]] = line.toList.traverse(v => Tile.of(v))
+      acc.flatMap(p => xs.map(_ :: p))
+    .map(_.reverse)
+
+def calculateNearestFreeSpots(platform: Platform): List[List[Int]] =
+  val initial = platform.head.map(_ => 0)
+  platform.zipWithIndex
+    .foldLeft(initial :: Nil):
+      case (acc, (row, idx)) =>
+        val adjustedRow = row
+          .zip(acc.head)
+          .map:
+            case ('.', previous) => previous
+            case ('O', previous) => previous + 1
+            case ('#', _)        => idx + 1
+        adjustedRow :: acc
+    .reverse
+    .tail
+
+def calculateLoad(platform: Platform) =
+  val height = platform.length
+  val spots  = calculateNearestFreeSpots(platform)
+
+  platform
+    .zip(spots)
+    .foldLeft(0):
+      case (acc, (row, spots)) =>
+        acc + row
+          .zip(spots)
+          .foldLeft(0):
+            case (acc, ('O', spot)) =>
+              acc + (height - spot + 1)
+            case (acc, _) => acc
+
+@main def run: Unit =
+  scala.io.Source
+    .fromFile("./day14.input")
+    .getLines()
+    .toList
+    .pipe(parse)
+    .pipe(_.map(calculateLoad))
+    // .pipe(solve(findReflectionIndex))
+    .pipe(println)
--- 2023/project.scala
+++ 2023/project.scala
@@ -1,1 +1,2 @@
+//> using dep "org.typelevel::cats-core:2.12.0"
 //> using test.dep org.scalameta::munit::1.0.0
